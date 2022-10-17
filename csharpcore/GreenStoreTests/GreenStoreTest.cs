278847f09e6704f49b99e784b83c0525ab854a91
Kurt C <87573560+TopcatAlphaDev@users.noreply.github.com>
2022-10-17 21:57:49 +0200

    UnitTest +3 agility vest values

--- csharpcore/GreenStoreTests/GreenStoreTest.cs
+++ csharpcore/GreenStoreTests/GreenStoreTest.cs
@@ -6,13 +6,20 @@ namespace GreenStoreTests
 {
     public class GreenStoreTest
     {
-        [Fact]
-        public void foo()
+
+        [Theory]
+        [InlineData(-1, 18)]
+        [InlineData(0, 18)]
+        [InlineData(5, 19)]
+        [InlineData(10, 19)]
+        [InlineData(100, 19)]
+        public void plus3_Agility_Vest_decreasing_quality(int sellIn, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "+3 Agility Vest", SellIn = sellIn, Quality = 20 } };
             GreenStore app = new GreenStore(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("+3 Agility Vest", Items[0].Name);
+            Assert.Equal(expectedQuality, Items[0].Quality);
         }
     }
 }
